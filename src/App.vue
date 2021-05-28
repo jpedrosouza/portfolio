79594ec5a800803ddc27d540389d6a5b8f7754cf
João Pedro Alves de Souza <jsouza.1650@gmail.com>
2021-05-27 23:05:36 -0300

    init home page layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 100;
 }
 
 #nav {
   padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="welcome-container">
+      <div class="welcome_message">
+         <span class="message_welcome">Bem-vindo <span class="non_negrite">ao</span></span>
+         <span class="message_below">nosso portfolio</span>
+      </div>
+      <div class="photos">
+        <img class="author_image" src="../assets/images/pedro.png" alt="Imagem de Pedro Henrique">
+        <img class="author_image" src="../assets/images/joao.png" alt="Imagem de João Pedro">
+        <img class="author_image" src="../assets/images/henrique.png" alt="Imagem de Henrique">
+        <img class="author_image" src="../assets/images/default_image.png" alt="Imagem default">
+        <img class="author_image" src="../assets/images/default_image.png" alt="Imagem default">
+        <img class="author_image" src="../assets/images/default_image.png" alt="Imagem default">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style>
+    .home {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .welcome-container {
+    width: 80%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 40px;
+
+  }
+
+  .welcome_message {
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+    font-weight: 700;
+  }
+
+  .non_negrite {
+    font-weight: 300;
+  }
+
+  .message_below {
+    font-weight: 300;
+  }
+
+  .photos {
+    width: 47%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  .author_image {
+    width: 100px;
+    height: 100px;
+    padding-bottom: 10px;
+  }
+</style>
