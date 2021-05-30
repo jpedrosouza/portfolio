9d022405d5c8fe04c496967540e0263f5b12ddd0
João Pedro Alves de Souza <jsouza.1650@gmail.com>
2021-05-30 17:23:33 -0300

    create hader component and add in home page

---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="header">
+        <div class="images">
+            <img src="../assets/images/logo.png" alt="Portfólio logo">
+            <img class="github_icon" src="../assets/images/github.png" alt="Github logo">
+        </div>
+        <div class="buttons">
+            <button class="menu_button"><span>PROJETOS</span></button>
+            <button class="menu_button"><span>SOBRE NÓS</span></button>
+            <button class="menu_button"><span>CONTATO</span></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header {
+        width: 90%;
+        padding-top: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: initial;
+    }
+
+    .images {
+        width: 15%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .github_icon {
+        width: 30px;
+        height: 30px;
+    }
+
+    .buttons {
+        width: 25%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .menu_button {
+        width: 100px;
+        height: 40px;
+        background: transparent;
+        border: none;
+    }
+
+    .menu_button :hover {
+        background: rgba(0, 0, 0, 0.123);
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <Header />
+
     <div class="welcome-container">
       <div class="welcome_message">
         <span class="message_welcome"
@@ -41,24 +43,40 @@
       </div>
     </div>
 
+    <img
+      class="down_icon"
+      src="../assets/icons/chevron-down.svg"
+      alt="Mais conteúdo abaixo"
+    />
+
     <Slider class="projects_slider" />
 
+    <div class="about_us">
+      <span class="about_title">Sobre nós</span>
+
+      <p class="about_text">
+        Somos um grupo de estudantes da Universidade do Vale do Itajaí |
+        Univali, cursando Análise e Desenvolvimento de Sistemas. Reunimos nesse
+        portfólio alguns de nossos projetos.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "../components/Header";
 import Slider from "../components/Slider";
 
 export default {
   name: "Home",
   components: {
     Slider,
+    Header,
   },
 };
 </script>
 
 <style>
-
 .home {
   display: flex;
   flex-direction: column;
@@ -68,6 +86,8 @@ export default {
 
 .welcome-container {
   width: 60%;
+  padding-top: 80px;
+  padding-bottom: 80px;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -108,4 +128,26 @@ export default {
   padding-top: 30px;
 }
 
+.down_icon {
+  padding-bottom: 40px;
+}
+
+.about_us {
+  width: 40%;
+  padding-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.about_title {
+  padding-bottom: 20px;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.about_text {
+  text-align: center;
+  font-size: 18px;
+}
 </style>
